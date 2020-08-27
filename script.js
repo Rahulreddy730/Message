f7fbfecaff9bd103f3f0de7a437c761f698da846
RahulReddy <easwerrahulreddy@gmail.com>
2020-08-27 10:33:40 +0100

    message

---
+++ script.js
@@ -0,0 +1,16 @@
+const sendBtn = document.querySelector('#sendBtn');
+const messageIn = document.querySelector('#messageIn');
+const messageOut = document.querySelector('#messageOut');
+
+sendBtn.addEventListener('click', sendMsg)
+
+function sendMsg(){
+    let content = messageIn.value;
+    if (content ===''){
+        alert('Please Enter Valid Value Current Value is Empty');
+    }else {
+        messageOut.innerHTML = content;
+        messageIn.value = '';
+    }
+
+}
